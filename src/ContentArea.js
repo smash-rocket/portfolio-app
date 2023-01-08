6df639802029b0763e01bbb865d25ee30ee54be2
Mia Pelino <miapelino1@gmail.com>
2023-01-08 00:40:10 -0800

    format landing page and sidebar

---
+++ src/ContentArea.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import LandingPage from './LandingPage';
+
+import './ContentArea.css';
+
+export default function ContentArea() {
+  return (
+      <div className="content-area">
+          <LandingPage />
+      </div>
+  );
+}
